4b72bc00f21333b622fad5ad44e39c49cb1e8ff3
Pawan Gola <pawangola908@gmail.com>
2024-06-06 15:08:26 +0530

    function declartion

---
+++ function/forward_declration.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+void sayHello();
+
+int main(){
+     sayHello();
+   return 0;
+}
+
+
+void sayHello(){
+    cout<<" Hello there \n"; 
+}
