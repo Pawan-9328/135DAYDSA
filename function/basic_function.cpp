fc5103e68d355f58de1d972da920215bae409035
Pawan Gola <pawangola908@gmail.com>
2024-06-06 15:07:35 +0530

    basic function ex

---
+++ function/basic_function.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+void sayHello(){
+    cout<<" Hello Everyone \n"; 
+}
+
+void assistant(){
+    cout<<"work done \n";
+}
+
+
+int main(){
+    assistant();
+   return 0;
+}
