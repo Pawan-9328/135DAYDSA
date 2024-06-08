37a0da875b43e83ac55fa6733130677db511cabe
Pawan Gola <pawangola908@gmail.com>
2024-06-08 13:41:29 +0530

    array basic operation

---
+++ Array/base_arr.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   //  int marks[50] = {1,2,3,4,5};
+   //  cout<<marks[0]<<endl;
+   //  cout<<sizeof(marks) / sizeof(int)<<endl;
+
+   //int arr[5];
+   int n; 
+   cout<<"Enter lenght of array ";
+   cin>>n;
+   int arr[n]; 
+
+   int len = sizeof(arr) / sizeof(int);
+   for (int i = 0; i < len; i++)
+   {
+      cin >> arr[i];
+   }
+   for (int idx = 0; idx < len; idx++)
+   {
+      cout << arr[idx] << " ";
+   }
+   cout << endl;
+   return 0;
+}
