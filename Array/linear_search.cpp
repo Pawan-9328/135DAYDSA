21863f2c203f1cceb4aef9f4d7a4a4cc8df4210a
Pawan Gola <pawangola908@gmail.com>
2024-06-08 14:51:34 +0530

    linear search

---
+++ Array/linear_search.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int linearSearch(int *arr, int n, int key)
+{
+   for (int i = 0; i < n; i++)
+   {
+      if (arr[i] == key)
+      {
+         return i;
+      }
+   }
+   return -1;
+}
+
+int main()
+{
+   int arr[] = {2, 4, 6, 8, 10};
+   int n = sizeof(arr) / sizeof(int);
+   cout << linearSearch(arr, n, 6) << endl;
+   return 0;
+}
